9f75f3ee371bc35397037e5faa277e2c9f761183
Vladimir Mychko <vmychko@rvision.ru>
2026-08-25 18:56:42 +0300

    disable UserServiceApplicationTests

--- user-service/src/test/kotlin/org/mychko/mytasktracker/UserServiceApplicationTests.kt
+++ user-service/src/test/kotlin/org/mychko/mytasktracker/UserServiceApplicationTests.kt
@@ -1,5 +1,6 @@
 package org.mychko.mytasktracker
 
+import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
@@ -7,6 +8,7 @@ import org.springframework.boot.test.context.SpringBootTest
 class UserServiceApplicationTests {
 
     @Test
+    @Disabled("Исключен из запуска: требует реальную БД. Для тестирования контроллеров используются изолированные @WebMvcTest")
     fun contextLoads() {
     }
 
