44ded11e164b78f1a0489215eee445641a34ec6d
Vladimir Mychko <vmychko@rvision.ru>
2026-09-04 18:14:22 +0300

    fix GlobalExceptionHandler

--- user-service/src/main/kotlin/org/mychko/mytasktracker/exception/GlobalExceptionHandler.kt
+++ user-service/src/main/kotlin/org/mychko/mytasktracker/exception/GlobalExceptionHandler.kt
@@ -1,16 +1,38 @@
 package org.mychko.mytasktracker.exception
 
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.media.Schema
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import org.slf4j.LoggerFactory
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestControllerAdvice
 import java.time.Instant
 
 @RestControllerAdvice
 class GlobalExceptionHandler {
 
+    companion object {
+        private val log = LoggerFactory.getLogger(GlobalExceptionHandler::class.java)
+    }
+
+
+    @ApiResponse(
+        responseCode = "404",
+        description = "Пользователь не найден",
+        content = [Content(
+            schema = Schema(
+                implementation = ErrorResponse::class
+            )
+        )]
+    )
+    @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(UserNotFoundException::class)
     fun handleUserNotFound(ex: UserNotFoundException): ResponseEntity<ErrorResponse> {
+        log.warn("User not found: {}", ex.message)
+
         val body = ErrorResponse(
             status = HttpStatus.NOT_FOUND.value(),
             error = HttpStatus.NOT_FOUND.reasonPhrase,
