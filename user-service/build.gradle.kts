1b6d3b6450b3eae36e480eedd9736f981788c300
Vladimir Mychko <vmychko@rvision.ru>
2026-09-24 21:52:17 +0300

    add flyway to user-service

--- user-service/build.gradle.kts
+++ user-service/build.gradle.kts
@@ -33,6 +33,8 @@ dependencies {
     implementation("org.apache.commons:commons-lang3:3.18.0")
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.16")
     implementation("com.github.loki4j:loki-logback-appender:2.0.3")
+    implementation("org.flywaydb:flyway-core:13.7.0")
+    implementation("org.flywaydb:flyway-database-postgresql:13.7.0")
     runtimeOnly("org.postgresql:postgresql")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
